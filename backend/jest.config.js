6e7cb5a5132c706a64024fc299dc61238b5daaa1
Md Ahaduzzzaman Ashik <65449096+ashikmdahad@users.noreply.github.com>
2025-09-18 21:07:05 +0200

    ADD: Dockerfile, docker compose file and package.json

---
+++ backend/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    testEnvironment: 'node',
+    collectCoverageFrom: [
+        'src/**/*.js',
+        '!src/**/*.test.js',
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov', 'html'],
+    testMatch: [
+        '**/src/**/*.test.js'
+    ],
+    setupFilesAfterEnv: ['<rootDir>/src/test-setup.js']
+};
